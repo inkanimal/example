56949d8f15ccc8b13c6bf6f4ff22acca726afc70
inkanimal <theatomproject@gmail.com>
2018-05-28 15:13:09 -0400

    further scraping

--- lib/weather/scraper.rb
+++ lib/weather/scraper.rb
@@ -8,8 +8,13 @@ class Scraper
      weather_content.each do |data|
        temp = data.css('.today_nowcard-temp').first.inner_text
        condition = data.css('.today_nowcard-phrase').first.inner_html
+       feels_temp = data.css('.today_nowcard-feels').first.inner_text
   end
 
   # new_content.css('.today_nowcard').css('.today_nowcard-temp').first.inner_text
   # new_content.css('.today_nowcard').css('.today_nowcard-phrase').first.inner_html
+  # new_content.css('.today_nowcard').css('.today_nowcard-feels').first.inner_text
+  # hilo and uv index = new_content.css('.today_nowcard').css('.today_nowcard-hilo').first.inner_text
+  # contains humidity, dew point, pressure, visibility may need to iterate over this and separate and put back together
+  # new_content.css('.today_nowcard').css('.today_nowcard-sidecar').first.inner_text
 end
