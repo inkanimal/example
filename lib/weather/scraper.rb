214bc3576a6ac708508d857570c1dc9c60827ee3
inkanimal <theatomproject@gmail.com>
2018-05-28 14:26:35 -0400

    update to scraper

--- lib/weather/scraper.rb
+++ lib/weather/scraper.rb
@@ -2,9 +2,12 @@ class Scraper
 
 
   def self.scrape_weather
+     web_content = open(https://weather.com/weather/today/l/11231:4:US)
+     doc = Nokogiri::HTML(web_content)
+     weather_content = doc.css('.today_nowcard')
 
-    doc = Nokogiri::HTML(open(https://weather.com/weather/today/l/11231:4:US))
-
-    doc.css('.today_nowcard-container').each do |conditions|    
+    doc.css('.today_nowcard-container').each do |conditions|
   end
+
+  new_content.css('.today_nowcard').css('.today_nowcard-temp').first.inner_text
 end
