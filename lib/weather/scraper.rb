3f488a20e37217cf0eaff0040b96c83c1d6eafa1
inkanimal <theatomproject@gmail.com>
2018-05-28 14:43:29 -0400

    more data conditions scraped

--- lib/weather/scraper.rb
+++ lib/weather/scraper.rb
@@ -5,9 +5,11 @@ class Scraper
      web_content = open(https://weather.com/weather/today/l/11231:4:US)
      doc = Nokogiri::HTML(web_content)
      weather_content = doc.css('.today_nowcard')
-
-    doc.css('.today_nowcard-container').each do |conditions|
+     weather_content.each do |data|
+       temp = data.css('.today_nowcard-temp').first.inner_text
+       condition = data.css('.today_nowcard-phrase').first.inner_html
   end
 
-  new_content.css('.today_nowcard').css('.today_nowcard-temp').first.inner_text
+  # new_content.css('.today_nowcard').css('.today_nowcard-temp').first.inner_text
+  # new_content.css('.today_nowcard').css('.today_nowcard-phrase').first.inner_html
 end
