0e2e32bed8461dd0b47c6f169f3c3be5c406f077
inkanimal <theatomproject@gmail.com>
2018-05-29 22:40:27 -0400

    CLI updating

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -1,5 +1,15 @@
 class Cli
 
-  def start
+  def call
+
+  end
+
+  def get_zip
+    input = ""
+    puts "Welcome to your Weather."
+    puts "Please enter your zip code to see the weather in your area. Type exit at any point to leave the application."
+    while input != "exit"
+    input = gets.strip
+
   end
 end
