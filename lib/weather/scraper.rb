465370fb99604f89f94b7de7a9f2bcb28d4eb5f2
inkanimal <theatomproject@gmail.com>
2018-06-01 13:29:15 -0400

    zips

--- lib/weather/scraper.rb
+++ lib/weather/scraper.rb
@@ -5,9 +5,7 @@ require 'pry'
 
 class Scraper
 
-
-
-  def self.scrape_weather
+ def self.scrape_weather
     #need to put #{zip_code} into the url
      web_content = open('https://weather.com/weather/today/l/11231:4:US')
      content = web_content.read
