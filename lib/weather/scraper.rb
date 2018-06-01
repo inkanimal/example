14c420d0564682871388f1bb883004a1c16f2623
inkanimal <theatomproject@gmail.com>
2018-05-31 21:45:57 -0400

    update scrape_weather

--- lib/weather/scraper.rb
+++ lib/weather/scraper.rb
@@ -5,31 +5,41 @@ require 'pry'
 
 class Scraper
 
+  @@all = []
 
   def self.scrape_weather
-    #need to put #{zip_code} into the url 
+    #need to put #{zip_code} into the url
      web_content = open('https://weather.com/weather/today/l/11231:4:US')
      content = web_content.read
      doc = Nokogiri::HTML(content)
      weather_content = doc.css('.today_nowcard')
-     weather_content.each do |data|
+     weather_content.collect do |data|
+       @@all <<
        temp = data.css('.today_nowcard-temp').first.inner_text
        condition = data.css('.today_nowcard-phrase').first.inner_html
        feels_temp = data.css('.today_nowcard-feels').first.inner_text
+     end
+  end
+
+  def self.scrape_weather_two
        # condition_text = data.css('.today_nowcard-sidecar').css('tr').css('th').collect do |item| item.text
-       condition_num  = data.css('.today_nowcard-sidecar').css('tr').css('td').collect do |item| item.text
-         condition_num
+     condition_num  = data.css('.today_nowcard-sidecar').css('tr').css('td').collect do |item| item.text
+       @@ll.push(condition_num)
+     end
+  end
+end
+
     wind = []
       wind.push(condition_num[0])
     humidity = []
       humidity.push(condition_num[1])
     dew_point = []
       dew_point.push(condition_num[2])
-    pressure = []
+    pressure
       pressure.push(condition_num[3])
-    visibility = []
+    visibility
       visibility.push(condition_num[4])
-    end
+
   # new_content.css('.today_nowcard').css('.today_nowcard-temp').first.inner_text
   # new_content.css('.today_nowcard').css('.today_nowcard-phrase').first.inner_html
   # new_content.css('.today_nowcard').css('.today_nowcard-feels').first.inner_text
@@ -43,6 +53,3 @@ class Scraper
   # .split(/(?<=\d)(?=[A-Za-z])/)
   # new_content.css('.today_nowcard').css('.today_nowcard-sidecar').css('tr').css('th').collect do |item| item.text
   # new_content.css('.today_nowcard').css('.today_nowcard-sidecar').css('tr').css('td').collect do |item| item.text
-  end
- end
-end
