f94982a85106696d0667636e96096a7b7aa19048
inkanimal <theatomproject@gmail.com>
2018-06-05 15:24:06 -0400

    refinement

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -32,8 +32,8 @@ class Cli
 
   def weather_list(weather_data)
     input = ""
-    while input != "9"
-      puts "Please type in the number that corresponds to the weather condition that you would like to view. To exit, please type 9."
+    while input != "10"
+      puts "Please type in the number that corresponds to the weather condition that you would like to view. To exit, please type 10."
       puts "            1. Temperature
             2. Current conditions
             3. Feels like Temperature
@@ -42,31 +42,34 @@ class Cli
             6. Dew Point
             7. Pressure
             8. Visibility
-            9. Exit"
+            9. Weather Summary
+            10. Exit"
 
       input = gets.strip
-      if input.to_i.between?(1,9) == false
+      if input.to_i.between?(1,10) == false
         puts "That is an invalid entry. Please choose again."
       else
       case input
 # instance
       when "1"
-        puts "The current temperature is #{weather_data.temp}."
+        puts "The current temperature is #{weather_data.get_temp}."
       when "2"
-        puts "The current weather condition is #{weather_data.condition}."
+        puts "The current weather condition is #{weather_data.get_condition}."
       when "3"
-        puts "The temperature #{weather_data.feels_temp}."
+        puts "The temperature #{weather_data.get_feels}."
       when "4"
-        puts "The current wind direction and speed are #{weather_data.wind.strip}."
+        puts "The current wind direction and speed are #{weather_data.get_wind.strip}."
       when "5"
-        puts "The humidity is #{weather_data.humidity}."
+        puts "The humidity is #{weather_data.get_hum}."
       when "6"
-        puts "The dew point is #{weather_data.dew_point}."
+        puts "The dew point is #{weather_data.get_dew}."
       when "7"
-        puts "The current pressure is #{weather_data.pressure.strip}."
+        puts "The current pressure is #{weather_data.get_pressure.strip}."
       when "8"
-        puts "#{weather_data.visibility} is the current visibility in your area."
+        puts "#{weather_data.get_vis} is the current visibility in your area."
       when "9"
+        puts weather_data.weather_summary
+      when "10"
         puts "Thank you for using Weather. Goodbye."
       else
         puts "Not a valid choice."
--- lib/weather/weather_main.rb
+++ lib/weather/weather_main.rb
@@ -2,27 +2,64 @@ class WeatherMain
 
   attr_accessor :temp, :condition, :feels_temp, :wind, :humidity, :dew_point, :pressure, :visibility
 
-  @@all = []
-
-  def initialize
-    @temp = temp
-    @condition = condition
-    @feels_temp = temp
-    @wind = wind
-    @humidity = humidity
-    @dew_point = dew_point
-    @pressure = pressure
-    @visibility = visibility
-    save
+  def get_temp
+    return temp
   end
 
-  def self.all
-    @@all
+  def get_condition
+    return condition
   end
 
-  def save
-    @@all << self
+  def get_feels
+    return feels_temp
   end
+
+  def get_wind
+    return wind
+  end
+
+  def get_hum
+    return humidity
+  end
+
+  def get_dew
+    return dew_point
+  end
+
+  def get_pressure
+    return pressure
+  end
+
+  def get_vis
+    return visibility
+  end
+
+  def weather_summary
+    return "The current weather in your area is #{get_temp} and #{get_condition}. Although, it #{get_feels}. The wind is #{get_wind.strip} and the current humidity is #{get_hum}. The dew point temp is #{get_dew}. #{get_vis} is the distance you can see today and the pressure is #{get_pressure.strip}."
+  end
+end
+
+# @@all = []
+
+  # def initialize
+    # @temp = temp
+  #   @condition = condition
+  #   @feels_temp = temp
+  #   @wind = wind
+  #   @humidity = humidity
+  #   @dew_point = dew_point
+  #   @pressure = pressure
+  #   @visibility = visibility
+  #   save
+  # end
+
+  # def self.all
+  #   @@all
+  # end
+  #
+  # def save
+  #   @@all << self
+  # end
 
   # def initialize(input = "")
   #   @input = input
@@ -40,4 +77,3 @@ class WeatherMain
   #     zip_code
   #   end
   # end
-end
